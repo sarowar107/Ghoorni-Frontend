e3f0efa573b07364b786799f0ce6db5118d45d30
Sarowar Islam <sarowar2204107@gmail.com>
2025-08-30 22:39:58 +0600

    backend api added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://ghoorni-backend-production.up.railway.app/api',
 });
 
 // Request Interceptor: Automatically add the JWT token to headers
